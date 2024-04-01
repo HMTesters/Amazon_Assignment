ca71e37b8b35bb8f3c531a98575434f43641b914
Saurabh Badave <saurabhbadave@gmail.com>
2024-04-01 14:01:43 +0530

    Scenario 4 Added

--- PageObjects/ProductSelectionPage.py
+++ PageObjects/ProductSelectionPage.py
@@ -13,6 +13,9 @@ class ProductSelection_Page:
     sort_option_xpath = 'Price: Low to High'
     sort_items_text_xpath = "//div[contains(@class,'a-section octopus-dlp-asin-title')]//a"
     prices_list = []
+    
+
+
 
     def __init__(self,driver):
         self.driver = driver
@@ -33,3 +36,19 @@ class ProductSelection_Page:
         all_products = self.driver.find_elements(By.XPATH,self.all_products_xpath)
 
         all_products[review_list.index(max(review_list))].click()
+
+
+    def sort_items_lowest_price(self):
+        self.driver.find_element(By.XPATH, self.dropdown_button_xpath).click()
+        time.sleep(5)
+        self.driver.find_element(By.LINK_TEXT, self.sort_option_xpath).click()
+        time.sleep(5)
+        prices = self.driver.find_elements(By.XPATH, self.find_items_prices_xpath)
+        time.sleep(4)
+        for x in prices:
+            self.prices_list.append(int(x.text))
+
+        self.sort_items_text = self.driver.find_elements(By.XPATH, self.sort_items_text_xpath)
+        self.sort_items_text[self.prices_list.index(min(self.prices_list))].click()
+        time.sleep(5)
+
--- Utilities/data.py
+++ Utilities/data.py
@@ -1,3 +1,11 @@
 accessories_text = 'Accessories'
 amazon_text = 'Amazon'
 clothing_text = 'Clothing'
+expected_free_delivery_text='FREE delivery'
+select_quantity='2'
+
+
+
+
+
+
